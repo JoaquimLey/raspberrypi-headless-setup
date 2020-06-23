c4ba3a6b79e566ae1457cc1cc608d69a1fdfbaab
Joaquim Ley <joaquimley@gmail.com>
2020-06-23 20:52:27 +0100

    Move credit to script

--- pi_bloat_remove.sh
+++ pi_bloat_remove.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre)
+# Original: https://github.com/raspberrycoulis/remove-bloat
 sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre -y
 
 # Autoremove
